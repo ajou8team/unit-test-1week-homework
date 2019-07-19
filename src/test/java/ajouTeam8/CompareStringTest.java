d4376e955965703fd8898425b1943caef963a607
MOHyeonMin <mhmrm@ajou.ac.kr>
2019-07-19 12:51:47 +0900

    make getTwoString_for_null_inuput method and testing assertTrue

--- src/test/java/ajouTeam8/CompareStringTest.java
+++ src/test/java/ajouTeam8/CompareStringTest.java
@@ -1,4 +1,23 @@
 package ajouTeam8;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class CompareStringTest {
+
+    private CompareString compareString ;
+
+
+    @Test
+    public void getTwoString_for_null_inuput(){
+        compareString = new CompareString();
+        String str1 =null;
+        String str2 =null;
+
+        //assertTrue
+        assertTrue(str1==null);
+
+        assertTrue(str2 ==null);
+    }
 }
