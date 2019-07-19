3ca112ae1afb4d0697ad0537643206ffc3289c1f
MOHyeonMin <mhmrm@ajou.ac.kr>
2019-07-19 12:49:42 +0900

    match line

--- src/main/java/ajouTeam8/CustomCalculator.java
+++ src/main/java/ajouTeam8/CustomCalculator.java
@@ -1,9 +1,7 @@
 package ajouTeam8;
 
 public class CustomCalculator {
-    public int add(int valueNum1, int valueNum2){
-        return valueNum1 + valueNum2;
-    }
+    public int add(int valueNum1, int valueNum2){return valueNum1 + valueNum2;    }
 
     public int subtract(int valueNum1, int valueNum2){
         return valueNum1 - valueNum2;
